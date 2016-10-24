ddd7936690f378957411e19d1f9873bd36e3c9e6
Chase <esmeraldarekz99@gmail.com>
2016-10-24 12:58:55 -0700

    save

--- Minesweeper/Minesweeper.java
+++ Minesweeper/Minesweeper.java
@@ -7,19 +7,30 @@
  */
 public class Minesweeper{
     //list 
-    int [] board; 
+    int board []; 
     int row;
     int column;
-     public void Minesweeper(int row, int column ){
-        board = new int[100];
-        row = row;
-        column = column;
-        for (int i = 0 ; i< row*column; i++){
-            board[i] = i + 1;
-    }
+     public Minesweeper(int row, int column ){
+       row = row;
+       column = column;
+       board = new int[row*column];
    }
    public Minesweeper(){
       this(10,10);
+    }
+   public void printSquareMinesweeper(){
+      System.out.println(">>>>>>>>>>Welcome To Minesweeper<<<<<<<<");
+      for (int i = 0; i < board.length; i++){
+         board[i] = i + 1;
       }
-}
-  
+    }
+   public void printBoard( int row, int column){
+      for (int r = 0; r <row; r++){
+          for (int c = 0; c< column; c++){
+              System.out.print("0");
+            }
+          System.out.println();
+            }
+         }
+      }
+    
