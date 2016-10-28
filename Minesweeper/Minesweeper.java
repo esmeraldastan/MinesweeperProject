82c95b9674dd09f7ed9224d7493142e2ffba8e00
Chase <esmeraldarekz99@gmail.com>
2016-10-28 12:59:06 -0700

    save

--- Minesweeper/Minesweeper.java
+++ Minesweeper/Minesweeper.java
@@ -6,12 +6,13 @@
  * @version 10.22.16
  */
 import java.util.Random;
+import java.lang.Math;
 public class Minesweeper{
     //list 
     int board []; 
     int row;
     int column;
-    int bombs []; 
+    int bombs; 
      public Minesweeper(int row, int column ){
        row = row;
        column = column;
@@ -31,14 +32,17 @@ public class Minesweeper{
        System.out.println();
        //number of rows and columns can be inserted 
       for (int r = 0; r <row; r++){
-          for (int c = 0; c< column; c++){
+          for (int c = 0; c < column; c++){
               System.out.print("*");
               //spacing
               if (row % 11 == 0){
                   System.out.println(" ");
              }
-             if ( column < 11 && column > 0){
+             
+                
+             if (column < 11 && column > 0){
                     System.out.print(" ");
+     
              }
           
             }
@@ -46,10 +50,16 @@ public class Minesweeper{
         }
      }
     public void addBombs( int bombs){
-       for ( bombs = -1; bombs < row*column; bombs++){
-           bombs = bombs + 1;
-           System.out.println(bombs.random[printBoard]);
-       }
+        while (bombs > 0){
+            this.bombs = bombs;
+            int posBombs = (int) (Math.random()*(row*column));
+            if (board[posBombs] == 0 ){
+                board[posBombs] = 9;
+                bombs = -1;
+             
+           }
+    }
    }
+  
 }
     
