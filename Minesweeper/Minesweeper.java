d47caa9f10ed20eaa0f75d8e9a620a3df2751c59
Chase <esmeraldarekz99@gmail.com>
2016-10-31 12:58:26 -0700

    save

--- Minesweeper/Minesweeper.java
+++ Minesweeper/Minesweeper.java
@@ -5,61 +5,61 @@
  * @author Esmeralda Lopez
  * @version 10.22.16
  */
-import java.util.Random;
+
 import java.lang.Math;
 public class Minesweeper{
     //list 
-    int board []; 
+    int [] board ; 
     int row;
     int column;
     int bombs; 
+    int posBombs;
      public Minesweeper(int row, int column ){
-       row = row;
-       column = column;
+       this.row = row;
+       this.column = column;
        board = new int[row*column];
    }
    public Minesweeper(){
       this(10,10);
-    }
-   public void printSquareMinesweeper(){
-      
-      for (int i = 0; i < board.length; i++){
-         board[i] = i + 1;
-      }
-    }
-   public void printBoard( int row, int column){
+            }
+   public void printBoard( ){
+       this.row=row;
+       this.column = column; 
+       
        System.out.println("     >>>>>>>>>>Welcome To Minesweeper<<<<<<<<");
-       System.out.println();
-       //number of rows and columns can be inserted 
-      for (int r = 0; r <row; r++){
-          for (int c = 0; c < column; c++){
-              System.out.print("*");
-              //spacing
-              if (row % 11 == 0){
-                  System.out.println(" ");
-             }
-             
-                
-             if (column < 11 && column > 0){
-                    System.out.print(" ");
+               System.out.println();
      
+       //number of rows and columns can be inserted 
+      for (int i= 0; i <board.length; i++){
+         //spacing
+         if (i %column == 0){
+                  System.out.println(" ");
+                  
              }
+         if (board[i] == -1) {
+                System.out.print(" *" );
+          }
+          
+         else{ 
+                System.out.print(board[i] + " " ) ;
+            }
+            }
           
             }
-            System.out.println();
-        }
-     }
+            //System.out.println();
+        
+     
     public void addBombs( int bombs){
         while (bombs > 0){
             this.bombs = bombs;
             int posBombs = (int) (Math.random()*(row*column));
-            if (board[posBombs] == 0 ){
-                board[posBombs] = 9;
-                bombs = -1;
+        
+            board[posBombs] = 9;
+            bombs = -1;
              
            }
     }
    }
   
-}
+
     
