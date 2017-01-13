985ebd74307bd4ef6bd3b896f9f81ebb8c299614
Chase <esmeraldarekz99@gmail.com>
2017-01-13 12:35:09 -0800

    save
    2D array

--- Minesweeper/NewMinesweeper.java
+++ Minesweeper/NewMinesweeper.java
@@ -8,34 +8,42 @@
 
 
 public class NewMinesweeper{
+    //2D array
     int [][] board;
     int row;
     int column;
     int bombs; 
     int posBombs;
-    public NewMinesweeper(){
-        row = row;
-        column = column;
+    public NewMinesweeper(int row, int column){
+        this.row = row;
+        this.column = column;
         board = new int [row][column];
     }
+    public NewMinesweeper(){
+        this(10,10);
+    }
     public void printBoard2(){
+        //title
         System.out.println("     >>>>>>>>>>Welcome To Minesweeper<<<<<<<<");
         System.out.println();
-        for (int i= 0; i <board.length; i++){
-            if (i %column == 0){
+        // no use of i
+        //instead row and columns 
+        for (int row= 0; row<board.length; row++){
+         
+            if (row %column == 0){
                 System.out.println(" ");
             }
-            if (board[i][i] == -1) {
+            if (board[row][row] == -1) {
                 // * represent bombs
                 System.out.print("* " );
             }
             else { 
-                System.out.print(board[i] + " " ) ;
+                System.out.print(board[row][row] + " " ) ;
             }
         }
     }
        }
-
+  
     
 
     
