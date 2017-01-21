ab614cedf66dcedc676396180862064e4d3fa960
Esmeralda C. Lopez <esmeraldarekz99@gmail.com>
2017-01-21 00:35:03 -0800

    save

--- Minesweeper/NewMinesweeper.java
+++ Minesweeper/NewMinesweeper.java
@@ -8,68 +8,103 @@
 //
 import java.lang.Math;
 public class NewMinesweeper{
-    //2D array
-    int [][] board;
-    int row;
-    int column;
-    int bombs; 
-    int XPos;
-    int YPos;
-    public NewMinesweeper(int row, int column){
-        this.row = row;
-        this.column = column;
+   //2D array
+   int [][] board;
+   int row;
+   int column;
+   //positions x,y
+   int XPos;
+   int YPos;
+   public NewMinesweeper(int rows, int columns){
+        this.row = rows;
+        this.column= columns;
         board = new int [row][column];
     }
-    public NewMinesweeper(){
+   public NewMinesweeper(){
         this(10,10);
     }
-    public void printBoard2(){
-        //title
-        this.row = row;
-        this.column = column;
-        System.out.println("     >>>>>>>>>>Welcome To Minesweeper<<<<<<<<");
-        System.out.println();
-        // no use of i
-        //instead row and columns 
-        for (row= 0; row<board.length-1; row++){
-            for (column = 0; column< board[0].length - 1 ; column++){
-          
-               
-          
-          if (board[row][column] == -1) {
+    //board print
+   public void printBoard2(){
+       System.out.println("     >>>>>>>>>>Welcome To Minesweeper<<<<<<<<");
+       System.out.println();
+       // no use of i
+       //instead row and column 
+        for (int rows= 0; rows < row; rows++){
+            for (int columns = 0; columns <column ; columns++){
+          if (board[rows][columns] == -1) {
                 // * represent bombs
                 System.out.print("* " );
           }
           else { 
-                System.out.print(board[row][column] + " " ) ;
+                System.out.print(board[rows][columns] + " " ) ;
           }
         }
         System.out.println( " ");
        }
     }
-    //bomb adding
-    public void addBombs(int bombs)throws Exception{
-        if ( bombs == 1){
+   //bomb adding
+   public void addBombs(int bombs)throws Exception{
+        if ( bombs < 1){
             throw new Exception(" Trying to cheat I see.... Not happening sucker!");
         }
         else if (bombs >= 101 ){
             throw new Exception( "Trying to kill your self?! That's great!");
         }
         int bombsplace = 0 ;
-         while (bombs < bombsplace){
-         
-          // random bomb placement 
-          //int posBombs = (int) (Math.random()*(row*column));
-          XPos=(int)(Math.random() * board.length -1);
-          YPos= (int)(Math.random() * board[0].length -1);
-            if (board[XPos][YPos]==0){
+         while (bombsplace < bombs){
+         XPos= (int)(Math.random() * row);
+         YPos= (int)(Math.random() * column);
+         //x,y
+         if (board[XPos][YPos]== 0){
             board[XPos][YPos] = -1;
             bombsplace++;
-
           }
         }
     }
+   // number adding
+   public void numbsAdded(){
+    // location of bombs/numbers on board 
+        for (int rows = 0; rows < row; rows++){
+            for (int columns = 0; columns < column; columns++){
+            if (board[rows][columns]== -1){
+            //right
+            if ( columns +1 < column  && board[rows][columns + 1] != -1){
+                    board[rows][columns + 1]++;
+             }
+            //left
+            if ( columns - 1 >= 0 && board[rows][columns - 1] != -1){
+                   board[rows][columns-1]++;
+             }
+            //up
+            if (rows - 1 >= 0 && board[rows - 1][columns] != -1){
+                 board[rows - 1][columns]++;
+                }
+            //down
+            if ( rows + 1 < row && board[rows + 1][columns]!= -1){
+                board[rows + 1][columns]++;
+            }
+            //up right 
+            if (rows - 1 >= 0 && columns + 1 < column && board[rows - 1][columns + 1] != -1){
+                board[rows-1][columns + 1]++; 
+            }
+            // up left 
+            if (rows - 1 >= 0 && columns - 1 >= 0 && board[rows - 1][columns - 1] != -1) {
+                board[rows -1][columns- 1]++; 
+            }
+            //down right 
+            if (rows + 1 < row && columns + 1 < column && board[rows + 1][columns + 1] != -1) {
+                board[ rows +1][columns+ 1]++; 
+            }
+            // down left
+             if (rows + 1 < row && columns - 1 >= 0 && board[rows + 1][columns - 1] != -1){
+                board[rows + 1][columns- 1]++; 
+            }
+          }
        }
+    }
+   }
+   
+}
   
     
 
